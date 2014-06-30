e44a57670b008cde0c624d046bdaca33eb67e81b
Christopher Gittner <christopher@gittner.org>
2014-06-30 21:55:07 +0200

    Code Cleanup

--- app/src/main/java/org/gittner/osmbugs/App.java
+++ app/src/main/java/org/gittner/osmbugs/App.java
@@ -3,9 +3,6 @@ package org.gittner.osmbugs;
 import android.app.Application;
 import android.content.Context;
 
-/**
- * Created by christopher on 3/20/14.
- */
 public class App extends Application {
 
     @Override
