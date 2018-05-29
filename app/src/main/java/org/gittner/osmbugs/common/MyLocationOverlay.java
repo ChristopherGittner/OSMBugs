673439115c676b4d0010b2eddb35dfce0d855c3d
Christopher Gittner <christopher@gittner.org>
2018-05-29 20:16:10 +0200

    Removed old unnused code

--- app/src/main/java/org/gittner/osmbugs/common/MyLocationOverlay.java
+++ app/src/main/java/org/gittner/osmbugs/common/MyLocationOverlay.java
@@ -1,27 +1,16 @@
 package org.gittner.osmbugs.common;
 
-import android.location.Location;
-
 import org.osmdroid.views.MapView;
-import org.osmdroid.views.overlay.mylocation.IMyLocationProvider;
 import org.osmdroid.views.overlay.mylocation.MyLocationNewOverlay;
 
 public class MyLocationOverlay extends MyLocationNewOverlay
 {
     private final FollowModeListener mListener;
 
-    private LocationChangedCallback mLocationChangedCallbacks = null;
-
-
     public MyLocationOverlay(MapView mapView, FollowModeListener listener)
     {
         super(mapView);
         mListener = listener;
-    }
-
-
-    public void addLocationChangedCallback(LocationChangedCallback mCallback) {
-        mLocationChangedCallbacks = mCallback;
     }
 
 
@@ -37,22 +26,5 @@ public class MyLocationOverlay extends MyLocationNewOverlay
     public interface FollowModeListener
     {
         void onFollowingStopped();
-    }
-
-
-    @Override
-    public void onLocationChanged(Location location, IMyLocationProvider source)
-    {
-        super.onLocationChanged(location, source);
-
-        if(mLocationChangedCallbacks != null) {
-            mLocationChangedCallbacks.onLocationChanged(location, source);
-        }
-    }
-
-
-    public interface LocationChangedCallback
-    {
-        void onLocationChanged(Location location, IMyLocationProvider source);
     }
 }
